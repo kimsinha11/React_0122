5cc8bb2a71efa35ccc42ae7f936062b1a0edf6c1
kimsinha11 <tlsgk2013@gmail.com>
2024-01-30 17:54:49 +0900

    react exam1

---
+++ src/about.js
@@ -0,0 +1,13 @@
+// about.js
+
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>안녕하세요. 소개 페이지입니다.</h2>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      <h2>안녕하세요. 연락 페이지입니다.</h2>
+    </div>
+  );
+}
+
+export default Contact;
